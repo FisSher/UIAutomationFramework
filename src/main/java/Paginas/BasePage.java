2b3f5e47c77b3be17b9b193f826935ab6b2cc75e
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-12-05 01:23:43 -0300

    startup

---
+++ src/main/java/Paginas/BasePage.java
@@ -0,0 +1,29 @@
+package Paginas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    static{
+        System.setProperty("webdriver.chrome.driver","\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10  );
+    }
+
+    public static void closeBrowser(){driver.quit();}
+    public static void navigateTo(String url){driver.get(url);}
+
+    private WebElement Find(String locator){
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+    }
+
+    public void clickElement(String locator)
+    {  Find(locator).click();  }
+}
